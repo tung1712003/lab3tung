4e2ac37cf21e0043607768451708e81bdf37e534
tung1712003 <lethanhtung1712003@gmail.com>
2023-12-04 22:15:00 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -3,12 +3,15 @@ from tkinter import ttk
 import random
 import pygame
 
-def close():
-    pygame.mixer.music.stop()
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 630
+
+def close(window, music):
+    music.stop()
     window.destroy()
 
-def clear():
-    hex_entry.delete(0, tk.END)  # Clear the Entry widget
+def clear(entry, key_label):
+    entry.delete(0, tk.END)  # Clear the Entry widget
     key_label.config(text="")   # Clear the result label
 
 def generate_key(hex_part):
@@ -16,16 +19,16 @@ def generate_key(hex_part):
     key = f"{dec_part // 1000:03d}-{dec_part % 1000:03d}-{random.randint(0, 999):03d} {random.randint(0, 99):02d}"
     return key
 
-def generate_button_clicked():
+def generate_button_clicked(hex_entry, key_label):
     hex_part = hex_entry.get()
     key = generate_key(hex_part)
     key_label.config(text=key)
 
 
-# Создаем главное окно и Устанавливаем изображение фона
+# Create the main window and set its properties
 window = tk.Tk()
 window.title("Keygen")
-window.geometry('1200x630')
+window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 bg_img = tk.PhotoImage(file='robot.png')
 
 lbl_bg = tk.Label(window, image=bg_img)
@@ -34,7 +37,7 @@ lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
 frame = tk.Frame(window)
 frame.place(relx=0.5, rely=0.5, anchor='center')
 
-#Создаем и располагаем виджеты
+# Create and place widgets
 hex_label = ttk.Label(frame, text="Enter HEX part:")
 hex_label.grid(column=0, row=0, padx=10, pady=15)
 
@@ -47,13 +50,13 @@ lbl_roots.grid(column=0, row=3, padx=10, pady=15)
 key_label = ttk.Label(frame, text="")
 key_label.grid(column=5, row=3, padx=10, pady=15)
 
-generate_button = ttk.Button(frame, text="Generate Key", command=generate_button_clicked)
+generate_button = ttk.Button(frame, text="Generate Key", command=lambda: generate_button_clicked(hex_entry, key_label))
 generate_button.grid(column=0, row=6, padx=10, pady=15)
 
-btn_exit = ttk.Button(frame, text='Exit', command=close)
+btn_exit = ttk.Button(frame, text='Exit', command=lambda: close(window, pygame.mixer.music))
 btn_exit.grid(column=5, row=6, padx=10, pady=15)
 
-btn_clear = ttk.Button(frame, text='Clear', command=clear)
+btn_clear = ttk.Button(frame, text='Clear', command=lambda: clear(hex_entry, key_label))
 btn_clear.grid(column=3, row=6, padx=10, pady=15)
 
 
@@ -64,5 +67,5 @@ pygame.mixer.music.load('background_music.mp3')
 pygame.mixer.music.play(-1)  # -1 means loop indefinitely
 
 
-# Запускаем главный цикл
+# Start the main event loop
 window.mainloop()
